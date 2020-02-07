7980bd311e7ca56998fa6f592ded07c4b8676981
jaberkow <jackberkowitz88@gmail.com>
2020-02-07 14:58:58 -0800

    added functionality for dropping long and misaligned recordings to build_nemo_manifest.py

--- build_nemo_manifest.py
+++ build_nemo_manifest.py
@@ -2,6 +2,54 @@ import os
 import argparse
 import pandas as pd
 
+def trim_df(df,max_duration=30.0,drop_bad=True):
+    """
+    Performs some cleaning on the dataframe, dropping recordings that
+    are too long, or where the source and noisy recording lengths differ
+
+    Inputs:
+        df - A pandas dataframe representing the index file of the dataset,
+            with the default columns of VOiCES index files.
+        max_duration - The maximum duration, in seconds, of recordings to keep
+            in the dataset (float)
+        drop_bad - If True, entries where the noisy recording is not the same
+            length as the source audio will be dropped
+    Outputs:
+        new_df - The cleaned dataframe
+    """
+    if drop_bad:
+        new_df = df[df['noisy_length']==df['source_length']]
+    else:
+        new_df = df.copy()
+    new_df = new_df[new_df['noisy_time']<=max_duration]
+    return new_df
+
+def convert_df_to_manifest(df,dataset_root):
+    """
+    Converts a dataframe in the format of a VOiCES index file to a JSON string
+    compatible with the NeMo ASR manifest format.
+
+    Inputs:
+    df - A pandas dataframe representing the index file of the dataset,
+        with the default columns of VOiCES index files.
+    dataset_root - A string with the absolute path to the root folder of the
+        VOiCES dataset.
+    Outputs:
+    record_string - A string representing the nemo manifest, in newline
+        delimited format.
+    """
+    # Keep only the filename, noisy recording time, and transcript columns
+    df = df[['filename','noisy_time','transcript']]
+    # Rename columns
+    df=df.rename(columns={"filename":"audio_filepath",
+    "noisy_time":"duration","transcript":"text"})
+    # Add the dataset root to every every entry in the filename column
+    df['audio_filepath'] = df['audio_filepath'].apply(lambda x: os.path.join(dataset_root,x))
+    # Output to JSON
+    record_string = df.to_json(orient='records',lines=True)
+    return record_string
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-r',dest='DATASET_ROOT',help='VOiCES dataset root',
@@ -10,6 +58,8 @@ if __name__ == '__main__':
     default='none',type=str)
     parser.add_argument('-o',dest='MANIFEST_PATH',help='Target file for the built manifest',
     default='none',type=str)
+    parser.add_argument('-m',dest='MAX_DURATION',help='Maximum allowed recording duration in seconds',
+    default=30.0,type=float)
     parser.add_argument('--drop_bad',dest='DROP_BAD',action='store_true')
     args = parser.parse_args()
 
@@ -26,16 +76,10 @@ if __name__ == '__main__':
 
     # load the index file
     df = pd.read_csv(INDEX_PATH,index_col='index')
-    # If DROP_BAD == True, drop all recordings of incorrect length
-    if args.DROP_BAD:
-        df =df[df['noisy_length']==df['source_length']]
 
-    # Keep only the filename, noisy recording time, and transcript columns
-    df = df[['filename','noisy_time','transcript']]
-    # Rename columns
-    df=df.rename(columns={"filename":"audio_filepath",
-    "noisy_time":"duration","transcript":"text"})
-    # Add the dataset root to every every entry in the filename column
-    df['audio_filepath'] = df['audio_filepath'].apply(lambda x: DATASET_ROOT+x)
-    # Output to JSON
-    df.to_json(args.MANIFEST_PATH,orient='records',lines=True)
+    trimmed_df = trim_df(df,max_duration=args.MAX_DURATION,
+    drop_bad=args.DROP_BAD)
+    json_string = convert_df_to_manifest(trimmed_df,DATASET_ROOT)
+    # Output to file
+    with open(args.MANIFEST_PATH,'w') as fout:
+        fout.write(json_string)
